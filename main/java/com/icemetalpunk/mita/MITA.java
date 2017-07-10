faec0eb992bfd7c77661621afebd8a99331b59e9
IceMetalPunk <icemetalpunk@gmail.com>
2017-07-10 01:19:25 -0400

    Renamed default code identifiers

--- main/java/com/icemetalpunk/mita/MITA.java
+++ main/java/com/icemetalpunk/mita/MITA.java
@@ -1,20 +1,16 @@
-package com.example.examplemod;
+package com.icemetalpunk.mita;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
-    }
+@Mod(modid = MITA.MODID, version = MITA.VERSION)
+public class MITA {
+	public static final String MODID = "mita";
+	public static final String VERSION = "0.1";
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+
+	}
 }
